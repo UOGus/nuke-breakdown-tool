2f2a58affce93f93e8e75310f64adcbd283d9caa
UOGus <166261586+UOGus@users.noreply.github.com>
2025-06-08 00:46:08 -0700

    Animated transition and added render button correctly

--- breakdown.py
+++ breakdown.py
@@ -52,21 +52,22 @@ class BreakdownPanel(nukescripts.PythonPanel):
 
         #knobs
         self.renderPath = nuke.File_Knob('render_path', 'file')
-        self.textBox = nuke.BBox_Knob('text_box', 'text area')
-        self.textBox.setVisible(False)
-        self.frame = nuke.Int_Knob('frame', 'frame')
+        self.frame = nuke.Int_Knob('frame', 'start frame')
         
         self.passList = nuke.PyCustom_Knob('pass_list', 'pass order', 'PassList()')
-        self.renderButton = nuke.PyScript_Knob("render_button", "render", "createTransition('Merge13', 'Merge20', (1,20))")
+        self.renderButton = nuke.PyScript_Knob("render_button", "render")
         
-
         #adding knobs to panel
         self.addKnob(self.renderPath)
-        self.addKnob(self.textBox)
         self.addKnob(self.frame)
         
         self.addKnob(self.renderButton)
         self.addKnob(self.passList)
+    #createTransition('Merge13', 'Merge20', (1,20))
+    def connectPasses(self):
+        passListWidget = self.passList.getObject()
+        passes = [passListWidget.list.item(x).text() for x in range(passListWidget.list.count())]
+        print(passes)
 
     def knobChanged(self, knob):
         #cases for the knobChanged callback
@@ -78,7 +79,8 @@ class BreakdownPanel(nukescripts.PythonPanel):
             case 'frame':
                 if self.frame.value() < 0:
                     raise ValueError("Frame must be non-negative")
-
+            case 'render_button':
+                self.connectPasses()
             case _:
                 pass
     
@@ -96,9 +98,9 @@ def createTransition(fromPass, toPass, range):
         return
     
     #init nodes for wipe transition
-    text = nuke.nodes.Text()
     merge = nuke.nodes.Merge()
     rect = nuke.nodes.CopyRectangle()
+    area = rect['area']
     frameRange = nuke.nodes.FrameRange()
     appendClip = nuke.nodes.AppendClip()
     
@@ -106,12 +108,10 @@ def createTransition(fromPass, toPass, range):
     format = a.format()
     width, height = format.width(), format.height()
 
-    text['message'].setValue(b.name())
-    text['box'].setValue([0,0, width, height])
 
     merge['operation'].setValue('over')
 
-    rect['area'].setValue([0, 0, 0, width])
+    rect['area'].setValue([0, 0, 0, height])
 
     frameRange['first_frame'].setValue(range[0])    
     frameRange['last_frame'].setValue(range[1])
@@ -120,13 +120,22 @@ def createTransition(fromPass, toPass, range):
     appendClip['lastFrame'].setValue(range[1])
 
     #connecteding nodes
-    merge.setInput(0, text)
-    merge.setInput(1, b)
+    merge.setInput(0, a)
     rect.setInput(0, merge)
-    rect.setInput(1, a)
+    rect.setInput(1, b)
     frameRange.setInput(0, rect)
     appendClip.setInput(0, frameRange)
-    print('success')
+
+    #animating transition
+    area.setAnimated()
+    #set key frame for width = 0 at first frame
+    area.setValueAt(0, range[0], 2)
+    area.setValueAt(height, range[0], 3)
+    #set key frame for width = full width at last frame
+    area.setValueAt(width, range[1], 2)
+    area.setValueAt(height, range[1], 3)
+    
+    return appendClip
 
 def addPanel():
     p = BreakdownPanel()
